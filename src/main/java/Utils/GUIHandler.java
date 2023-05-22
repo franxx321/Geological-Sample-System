319e2d9c601b91f4c7bc90cb1e7123f77b24d07a
franxx321 <francruy@gmail.com>
2023-05-22 08:19:33 -0300

    Added Gui Handler and Object Adder

---
+++ src/main/java/Utils/GUIHandler.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utils;
+
+import java.util.HashMap;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author franc
+ */
+public class GUIHandler {
+    private HashMap<String,JFrame> frames;
+    private JFrame currentFrame;
+
+    public GUIHandler() {
+        this.frames= new HashMap();
+        //TODO cargar los Frames al hashmap, crear variables estaticas para acceder los hashmaps, asignar como currentFrame al frame del menu.
+        
+    }
+    
+    
+    
+    
+    public void changeFrame(String nextFrame){
+        this.currentFrame.setVisible(false);
+        this.currentFrame= frames.get(nextFrame);
+        this.currentFrame.setVisible(true);
+        this.currentFrame.repaint();
+        this.currentFrame.revalidate();
+    }
+    
+}
