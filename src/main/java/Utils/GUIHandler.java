ea8a463c42278b2844566dbcd1ec3fa714018f22
franxx321 <francruy@gmail.com>
2023-05-26 22:15:13 -0300

    //TODO corregir errores de cast en los botones del menu (Casteando un table displayer a arithmetic displayer), Corregir un error en el boton de personas y cantidad de objetos, conectar OiBTG y DOTG, agregar carteles del formato de fecha

--- src/main/java/Utils/GUIHandler.java
+++ src/main/java/Utils/GUIHandler.java
@@ -23,14 +23,13 @@ public class GUIHandler {
     public GUIHandler(Menu menu) {
         this.frames= new HashMap();
         this.currentFrame=menu;
-        //TODO cargar los Frames al hashmap, crear variables estaticas para acceder los hashmaps, asignar como currentFrame al frame del menu.
         this.frames.put(GUIHandler.menu ,menu);
-        this.frames.put(GUIHandler.tableDisplayer,new TableDisplayer());
-        this.frames.put(GUIHandler.arithmeticDisplayer,new ArithmeticDisplayer());
-        this.frames.put(GUIHandler.boxDeleter,new BoxDeleter());
-        this.frames.put(GUIHandler.objectAdder,new ObjectAdder());
-        this.frames.put(GUIHandler.DOTP,new DOTP());
-        this.frames.put(GUIHandler.OiBTP,new OiBTP());
+        this.frames.put(GUIHandler.tableDisplayer,new TableDisplayer(this));
+        this.frames.put(GUIHandler.arithmeticDisplayer,new ArithmeticDisplayer(this));
+        this.frames.put(GUIHandler.boxDeleter,new BoxDeleter(this));
+        this.frames.put(GUIHandler.objectAdder,new ObjectAdder(this));
+        this.frames.put(GUIHandler.DOTP,new DOTP(this));
+        this.frames.put(GUIHandler.OiBTP,new OiBTP(this));
     }
     
     
